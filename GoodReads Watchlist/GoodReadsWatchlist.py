874001e17dde85c1e7c9362e97440336f3bd9597
Krayakin <krayakin@gmail.com>
2014-10-23 20:13:12 -0700

    tsting

--- GoodReads Watchlist/GoodReadsWatchlist.py
+++ GoodReads Watchlist/GoodReadsWatchlist.py
@@ -1,6 +1,6 @@
 from requests_oauthlib import OAuth1Session
 
-import xml2dict
+import xmltodict
 
 from xdm.plugins import *
 
@@ -13,7 +13,7 @@ baseURL = 'http://www.goodreads.com'
 #unwatchlistShowURL = baseURL + "show/unwatchlist/"
 
 class goodreadswatchlist(MediaAdder):
-    version = "0.206"
+    version = "0.207"
     identifier = "com.krayakin.goodreadswatchlist"
     addMediaTypeOptions = False
     screenName = 'GoodReads Watchlist'
@@ -98,7 +98,7 @@ class goodreadswatchlist(MediaAdder):
         searchUrl = '%s/review/list' % baseURL
         r = oauth.get(searchUrl, params=payload)
         #r = requests.get(searchUrl, params = payload)
-        log.debug('' + xml2dict.parse(r.text))
+        log.debug('' + xmltodict.parse(r.text))
         #shows = self._getWatchlist(showWatchlistURL, self.c.username, self.c.password, self.c.apikey)
         #for show in shows:
         #    additionalData = {}
